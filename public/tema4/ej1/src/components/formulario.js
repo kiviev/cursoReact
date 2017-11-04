b488b2ba893e61bbb5e46421c7458b0a8a4052e8
Paco Alba <kiviev@gmail.com>
2017-11-04 17:49:21 +0100

    Tema 4 ejercicio 1

--- public/tema4/ej1/src/components/formulario.js
+++ public/tema4/ej1/src/components/formulario.js
@@ -1,32 +1,64 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 class Formulario extends React.Component{
 
 	constructor(props){
 		super(props);
 		this.state = {
-			value : ''
+			value : '',
+			form : {
+				name : '',
+				surname : '',
+				age : '',
+				prof : ''
+			},
+			toString : ''
 		};
 	}
 
+
 	handleChanges(ev){
-		this.setState({value : ev.target.value});
+		let formx = this.state.form;
+		formx[ev.target.id] = ev.target.value;
+		this.setState({form : formx});
 	}
 
 	handleSend(ev){
-		console.log('Enviado: ' + this.state.value);
+		console.log(this.state.form);
+		this.setState({toString : this.toString()})
 		ev.preventDefault();
+		
 	}
 
+	toString(){
+		return ("Nombre: " + this.state.form.name + ", Apellidos: " + this.state.form.surname + ", Edad: " + this.state.form.age + ", Profesion: " + this.state.form.prof + ".");
+	}
 
 	render(){
 
 		return(
 			<form onSubmit={this.handleSend.bind(this)}>
-				<label>Nombre:</label>
-					<input type="text" value={this.state.value}
+				<div className='form-group' >
+					<label>Nombre:</label>
+					<input type="text" value={this.state.form.name} id='name'
+							onChange={this.handleChanges.bind(this)} />
+				</div>
+				<div className='form-group' >
+					<label>Apellidos:</label>
+					<input type="text" value={this.state.form.surname} id='surname'
 						onChange={this.handleChanges.bind(this)} />
-					<input type="submit" value="Enviar" />
+				</div>
+				<div className='form-group' >
+					<label>Edad:</label>
+					<input type="number" value={this.state.form.age} id='age'
+							onChange={this.handleChanges.bind(this)} />
+				</div>
+				<div className='form-group' >
+					<label>Profesion:</label>
+					<input type="text" value={this.state.form.prof} id='prof'
+							onChange={this.handleChanges.bind(this)} />
+				</div>
+				<input type="submit" value="Enviar" />
+				<p>{this.state.toString}</p>
 			</form>
 			);	
 
