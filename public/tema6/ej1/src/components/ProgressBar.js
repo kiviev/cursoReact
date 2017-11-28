259caab986b581d4d943d2e783006631e19e0b2a
Paco Alba <kiviev@gmail.com>
2017-11-28 20:34:26 +0100

    Tema6 Ejercicios 1 y 2 terminados

--- public/tema6/ej1/src/components/ProgressBar.js
+++ public/tema6/ej1/src/components/ProgressBar.js
@@ -4,6 +4,7 @@ import './foundation.min.css';
 
 // foundation
 import { ProgressBar } from 'react-foundation-components/lib/progress-bar';
+import { Thumbnail } from 'react-foundation-components/lib/thumbnail';
 class ProgressBarClass extends React.Component {
 
 constructor(props){
@@ -15,33 +16,52 @@ constructor(props){
 
 	this.params = {
 		max : 100,
-		min: 0
+		min: 0,
+		interval : 200
 	}
+}
+
+randomNumber(min,max){
+	return Math.floor(Math.random()*(max-min+1)+min);
 }
 
  complexFormatter(percent, value, min, max) {
 return `percent = ${percent}, value = ${value}, min = ${min}, max = ${max}`;
 }
 
-time(){return Math.round(Math.random() * 1000)};
  percentFormatter(percent) {
-const rounded = Math.round(percent * 100);
- 		let progress = this.state.percent;
+	const rounded = Math.round(percent * 100);
+	let progress = this.state.percent;
+	let max = this.params.max;
 
- 	let timer = setInterval(()=>{
- 		if ( progress <= this.state.percent && progress <= this.params.max){
- 			this.setState({percent : progress ++});
- 			console.log('porc: ' + this.state.percent);
- 		}//else clearInterval(timer);
- 	} , this.time()).bind(this);
+	let interval = setInterval(()=>{
+		if(progress <= max){
+			let rand = this.randomNumber(1,3);
+			progress = progress + rand;
+			if (progress >= this.params.max){
+				progress = this.params.max;
+			}	clearInterval(interval);
+			this.setState({percent : progress})
+		}
+	},this.params.interval);
 
 return `${rounded}%`;
 }
 
 
 render(){
+	const thumpStyle ={
+		display : this.state.percent >= this.params.max ? 'block' : 'none'
+	}
 	return (
+		<div>
 			<ProgressBar color="alert" labelFormatter={this.percentFormatter.bind(this)} max={this.params.max} min={this.params.min} value={this.state.percent} />
+			<Thumbnail style={thumpStyle}
+					alt="Photo of Uranus."
+					width="100%"
+					src="https://www.viajejet.com/wp-content/viajes/paisajes-del-campo-flores.jpg"
+					/>
+		</div>
 		)
 }
 
