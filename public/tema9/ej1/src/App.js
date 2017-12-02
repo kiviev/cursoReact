5c36d33d3261cc116bf306637563c4c314ae1fa8
Paco Alba <kiviev@gmail.com>
2017-12-02 13:30:12 +0100

    Tema9 ejercicios 1, 2 y preparacion para ej 3

---
+++ public/tema9/ej1/src/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { OrderedSet } from 'immutable';
+import { NotificationStack } from 'react-notification';
+
+import Slider from 'react-slick';
+import SlidesData from './slides.js';
+import './slider.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      notifications : OrderedSet(),
+      count : 0
+    }
+    this.removeNotification=this.removeNotification.bind(this);
+    this.images = SlidesData;
+
+  }
+
+  addNotification() {
+    const { notifications, count } = this.state;
+    const id = notifications.size + 1;
+    const newCount = count + 1;
+    return this.setState({
+      count: newCount,
+      notifications: notifications.add({
+          message: `Mi notificacion ${id}`,
+          key: newCount,
+          action: 'Close',
+          dismissAfter: 4000,
+          onClick: () => this.removeNotification(newCount),
+      })
+    });
+  }
+
+  removeNotification (count) {
+    const { notifications } = this.state;
+    this.setState({
+      notifications: notifications.filter(n => n.key !== count)
+    })
+  }
+
+  render () {
+    let settings = {
+      dots: true,
+      speed: 500,
+      adaptiveHeight : true,
+      arrows : true,
+      slidesToScroll: 1,
+       centerMode : true
+    };
+    return (
+      <div>
+        <div className="notificaciones">
+          <button onClick={this.addNotification.bind(this)}>
+            Añadir notificación
+          </button>
+          <NotificationStack
+            notifications={this.state.notifications.toArray()}
+            onDismiss={notification => this.setState({
+                notifications:this.state.notifications.delete(notification)
+              })}
+          />
+        </div>
+        <div className="slider">
+          <Slider  {...settings}>
+            {
+              this.images.map((image) => {
+                  return ( <div className="slide"><img src={image.src} alt={image.alt}></img></div>)
+              })
+            }
+          </Slider>
+        </div>
+      </div>
+      );
+    }
+}
+
+export default App;
---
+++ public/tema9/ej1/src/slides.js
@@ -0,0 +1,28 @@
+const slidesData = [
+	{
+		"src" : "https://i.ytimg.com/vi/_tQfUK335xk/maxresdefault.jpg",
+		"alt" :  "Lago"
+	},
+	{
+		"src" : "http://www.paisajesimagenes.com/wp-content/uploads/fotos-de-paisajes.jpg",
+		"alt" :  "Arroyo"
+	},
+	{
+		"src" : "http://1.bp.blogspot.com/-5eD4rSBm7XU/VYIEZ8SK9JI/AAAAAAAAB0w/BbcUVXzUxJY/s1600/colores_nocturnos-1024x768.jpg",
+		"alt" :  "Luna"
+	},
+	{
+		"src" : "https://www.hola.com/imagenes/viajes/2016021083709/pueblos-montana-bonitos-espana/0-351-436/a_canga-a.jpg",
+		"alt" :  "Puente"
+	},
+	{
+		"src" : "https://s-media-cache-ak0.pinimg.com/originals/05/33/b4/0533b49b75ba4368af88fe341e7384c5.jpg",
+		"alt" :  "Margaritas"
+	},
+	{
+		"src" : "https://kdeotero.files.wordpress.com/2015/05/fondos-pantalla-paisajes-hermosos-bonitos-naturales-escritorio-fotografias-hd-wallpaper-imagenes-fotos-3d-tailandia-playas-bonito-viajar-1.jpg",
+		"alt" :  "Barca"
+	}
+]
+
+export default slidesData;
